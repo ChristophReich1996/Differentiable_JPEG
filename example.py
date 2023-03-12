5e7633d8bea04577d5d55acd5aab4cc5c737bae7
ChristophReich1996 <christophreich@gmx.net>
2023-03-13 00:48:56 +0100

    Use different compression strengths

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ def coding() -> None:
     # Load test image and reshape to [B, 3, H, W]
     image: Tensor = torchvision.io.read_image("test_images/00000000.png").float()[None]
     # Init compression
-    compression_strength: Tensor = torch.tensor([99], requires_grad=True, dtype=torch.float)
+    compression_strength: Tensor = torch.tensor([70], requires_grad=True, dtype=torch.float)
     # Perform coding
     image_jpeg: Tensor = jpeg_coding(image_rgb=image, compression_strength=compression_strength, differentiable=True)
     # Print L1 distance
@@ -27,7 +27,7 @@ def coding_class() -> None:
     # Load test image and reshape to [B, 3, H, W]
     image: Tensor = torchvision.io.read_image("test_images/00000000.png").float()[None]
     # Init compression
-    compression_strength: Tensor = torch.tensor([99], requires_grad=True, dtype=torch.float)
+    compression_strength: Tensor = torch.tensor([55], requires_grad=True, dtype=torch.float)
     # Init JPEG coding class
     jpeg_coding_module: nn.Module = JPEGCoding(differentiable=True)
     # Perform coding
@@ -40,11 +40,12 @@ def coding_class() -> None:
     plt.imshow(image_jpeg[0].permute(1, 2, 0).detach().clip(min=0, max=255) / 255.0)
     plt.show()
 
+
 def encode_decode() -> None:
     # Load test image and reshape to [B, 3, H, W]
     image: Tensor = torchvision.io.read_image("test_images/00000000.png").float()[None]
     # Init compression
-    compression_strength: Tensor = torch.tensor([99], requires_grad=True, dtype=torch.float)
+    compression_strength: Tensor = torch.tensor([5], requires_grad=True, dtype=torch.float)
     # Perform encoding
     y_encoded, cb_encoded, cr_encoded = jpeg_encode(image, compression_strength, True)
     # Perform decoding
